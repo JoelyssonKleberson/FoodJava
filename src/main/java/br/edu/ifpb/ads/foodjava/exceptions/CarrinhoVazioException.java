1f11997a086f53a48b92fe06fbe36a3416d45887
Joelysson Kleberson <joelysson.kleberson.ferreira@gmail.com>
2026-06-17 17:08:53 -0300

    Criando a exceção do carrinho vazio

---
+++ src/main/java/br/edu/ifpb/ads/foodjava/exceptions/CarrinhoVazioException.java
@@ -0,0 +1,7 @@
+package br.edu.ifpb.ads.foodjava.exceptions;
+
+public class CarrinhoVazioException extends RuntimeException {
+    public CarrinhoVazioException(String message) {
+        super(message);
+    }
+}
