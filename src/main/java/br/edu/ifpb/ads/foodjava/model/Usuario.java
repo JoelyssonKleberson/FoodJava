249957a6e9f2ee9d381212b776ee7c5ff25c8f3e
Joelysson Kleberson <joelysson.kleberson.ferreira@gmail.com>
2026-06-19 15:25:46 -0300

    Implementação dos métodos get, set, equals e hashCode da classe

--- src/main/java/br/edu/ifpb/ads/foodjava/model/Usuario.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/model/Usuario.java
@@ -1,5 +1,7 @@
 package br.edu.ifpb.ads.foodjava.model;
 
+import java.util.Objects;
+
 public abstract class Usuario {
 
     private String nome;
@@ -10,5 +12,41 @@ public abstract class Usuario {
         this.nome = nome;
         this.email = email;
         this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Usuario usuario = (Usuario) o;
+        return Objects.equals(email, usuario.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(email);
     }
 }
