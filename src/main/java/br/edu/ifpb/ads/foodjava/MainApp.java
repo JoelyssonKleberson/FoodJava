ea61f4fa19c8c73708de5dee366e70f833901410
Joelysson Kleberson <joelysson.kleberson.ferreira@gmail.com>
2026-07-02 11:02:40 -0300

    Implementando a tela de carregamento ao Main

--- src/main/java/br/edu/ifpb/ads/foodjava/MainApp.java
+++ src/main/java/br/edu/ifpb/ads/foodjava/MainApp.java
@@ -1,6 +1,6 @@
 package br.edu.ifpb.ads.foodjava;
 
-import br.edu.ifpb.ads.foodjava.view.TelaLogin;
+import br.edu.ifpb.ads.foodjava.view.TelaSplash;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,24 +9,26 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage stage) {
-        TelaLogin loginView = new TelaLogin();
-        // Ajustamos as proporções iniciais para combinar com a nova interface centralizada
-        Scene cenaInicial = new Scene(loginView.getLayout(), 450, 550);
+        // 1. Instancia a tela de Splash
+        TelaSplash splashView = new TelaSplash();
 
+        // 2. Cria a cena principal com a resolução travada
+        Scene cenaInicial = new Scene(splashView.getLayout(), 1100, 700);
+
+        // 3. Configuração do Stage (Janela)
         stage.setTitle("FoodJava");
         stage.setScene(cenaInicial);
 
-        // --- MUDANÇAS AQUI ---
-        stage.setMaximized(false); // Faz a tela abrir pegando o monitor inteiro
-        stage.setResizable(true); // Devolve os botões de minimizar e maximizar da janela
+        // Trava o redimensionamento para manter o layout perfeito e igual à referência
+        stage.setResizable(false);
 
         stage.show();
+
+        // 4. Inicia a transição de carregamento
+        splashView.iniciarTransicaoParaLogin(stage);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
-
-
